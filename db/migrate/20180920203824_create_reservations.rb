0bbf22e7528a98cda12aff62aa3a70b5739b0e40
aymanthefirst <AHarake@spartaglobal.com>
2018-09-20 21:44:42 +0100

    got to line 66

---
+++ db/migrate/20180920203824_create_reservations.rb
@@ -0,0 +1,12 @@
+class CreateReservations < ActiveRecord::Migration[5.2]
+  def change
+    create_table :reservations do |t|
+      t.string :date
+      t.integer :user_id
+      t.integer :room_id
+      t.integer :id
+
+      t.timestamps
+    end
+  end
+end
